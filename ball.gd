74a0f3a5198a9a2848db8464640f68c8bb56c75a
AnsonRutherford <pvtgeekhook@gmail.com>
2025-10-03 20:51:05 -0400

    vector preview

--- ball.gd
+++ ball.gd
@@ -7,6 +7,8 @@ extends RigidBody3D
 @onready var shoot_button = %ShootButton
 @onready var debug_info = %DebugInfo
 
+@onready var path_preview = %PathPreview
+
 const height_values = [1, 2, 3]
 
 func _ready() -> void:
@@ -21,11 +23,21 @@ func calculate_shot() -> Vector3:
 	return Vector3(sin(angle_rad), get_height(), cos(angle_rad)) * power
 
 func _process(_delta: float) -> void:
+	var shot = calculate_shot()
+	path_preview.position = shot / 2
+	var shot_length = shot.length()
+	if shot_length > 0:
+		path_preview.global_basis = Basis.looking_at(shot)
+	path_preview.scale.z = shot_length
+	path_preview.scale.x = 0.5 + shot_length * 0.2
+	
 	var info_text = "height: %s\n" % height_slider.value
 	info_text += "power: %s\n" % power_slider.value
 	info_text += "aim: %s\n" % aim_slider.value
-	info_text += "shot vector: %s" % calculate_shot()
+	info_text += "shot vector: %s" % shot
+	info_text += "path preview scale: %s" % path_preview.scale
 	debug_info.text = info_text
 
 func _shoot_button_is_pressed() -> void:
 	apply_impulse(calculate_shot())
+	path_preview.visible = false
