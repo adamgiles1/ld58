8e8f4e039999886eeba55bce2bcbfd5fb774708f
Adam Giles <adamag@cox.net>
2025-10-03 20:38:58 -0400

    Demo exists

--- ball.gd
+++ ball.gd
@@ -7,17 +7,20 @@ extends RigidBody3D
 @onready var shoot_button = %ShootButton
 @onready var debug_info = %DebugInfo
 
-const height_values = [1, 2, 3]
+const height_values = [.25, .5, 1.0]
+var reset_point: Vector3
+var reset_velocity_on_next_frame := false
 
 func _ready() -> void:
 	shoot_button.pressed.connect(_shoot_button_is_pressed)
+	reset_point = global_position
 
 func get_height() -> float:
 	return height_values[height_slider.value]
 
 func calculate_shot() -> Vector3:
 	var angle_rad = aim_slider.value * PI/50
-	var power = power_slider.value * 0.1
+	var power = power_slider.value * 0.01
 	return Vector3(sin(angle_rad), get_height(), cos(angle_rad)) * power
 
 func _process(_delta: float) -> void:
@@ -26,6 +29,19 @@ func _process(_delta: float) -> void:
 	info_text += "aim: %s\n" % aim_slider.value
 	info_text += "shot vector: %s" % calculate_shot()
 	debug_info.text = info_text
+	
+	if Input.is_action_just_pressed("ResetBall"):
+		reset()
+
+func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	if reset_velocity_on_next_frame:
+		reset_velocity_on_next_frame = false
+		state.linear_velocity = Vector3.ZERO
+		state.angular_velocity = Vector3.ZERO
+		global_position = reset_point
 
 func _shoot_button_is_pressed() -> void:
 	apply_impulse(calculate_shot())
+
+func reset() -> void:
+	reset_velocity_on_next_frame = true
--- level_pieces/hoop.gd
+++ level_pieces/hoop.gd
@@ -1,16 +1,20 @@
 extends Node3D
 
 @onready var area: Area3D = $Area3D
+@onready var mesh: MeshInstance3D = $MeshInstance3D
+var material: StandardMaterial3D
 
 var is_hit: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	material = mesh.get_active_material(0)
 	area.body_entered.connect(on_ball_collide)
 
-func on_ball_collide():
+func on_ball_collide(body: Node3D):
 	if is_hit:
 		return
 	print("ball hit ring")
 	is_hit = true
 	Signals.HOOP_HIT.emit()
+	mesh.material_override = preload("res://level_pieces/hoop_material_hit.tres")
